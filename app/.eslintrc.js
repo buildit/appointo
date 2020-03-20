9b79ec6d7d524f5ceeb42317dbeafd3ce6e23ae2
anshad <anshad.musafir@gmail.com>
2020-03-20 16:39:03 +0530

    Update lint and add required packages

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,4 +1,21 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['plugin:@typescript-eslint/recommended', '@react-native-community'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier', 'simple-import-sort'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    },
+    'import/resolver': {
+      typescript: {}
+    }
+  },
+  rules: {
+    'comma-dangle': ['error', 'never'],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'simple-import-sort/sort': 'error',
+    'sort-imports': 'off',
+    'prettier/prettier': 'error'
+  }
 };
--- app/.prettierrc.js
+++ app/.prettierrc.js
@@ -2,5 +2,7 @@ module.exports = {
   bracketSpacing: false,
   jsxBracketSameLine: true,
   singleQuote: true,
-  trailingComma: 'all',
+  trailingComma: "none",
+  tabWidth: 4,
+  semi: true
 };
